f52318c313d726937188ec90d1ccafadab799ed9
Trofim Chuchuk <trofim.chuchuk@mail.ru>
2024-01-29 21:12:46 +0700

    first commit

---
+++ ModernVPN/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ModernVPN.Core;
+using ModernVPN.MVVM.View;
+using ModernVPN.MVVM.ViewModel;
+using System.Windows;
+
+namespace ModernVPN.MVVM.ViewModel {
+    public class MainViewModel : ObservableObject {
+        private object _currentView;
+        public RelayCommand MoveWindowsCommand { get; set; }
+        public RelayCommand ShutdownWindowsCommand { get; set; }
+        public RelayCommand MaximizeWindowsCommand { get; set; }
+        public RelayCommand MinimizeWindowsCommand { get; set; }
+
+
+        public RelayCommand ShowProtectionView { get; set; }
+        public RelayCommand ShowSettingsView { get; set; }
+
+
+        public object CurrentView {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ProtectionViewModel ProtectionVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+
+
+        public MainViewModel() {
+
+            ProtectionVM = new ProtectionViewModel();
+            SettingsVM = new SettingsViewModel();
+            CurrentView = ProtectionVM;
+            Application.Current.MainWindow.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MoveWindowsCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
+            ShutdownWindowsCommand = new RelayCommand(o => { Application.Current.Shutdown(); });
+            MaximizeWindowsCommand = new RelayCommand(o => {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized) {
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                }
+                else {
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                }
+            });
+            MinimizeWindowsCommand = new RelayCommand(o => { Application.Current.MainWindow.WindowState = WindowState.Minimized; });
+
+            ShowProtectionView = new RelayCommand(o => { CurrentView = ProtectionVM; });
+            ShowSettingsView = new RelayCommand(o => { CurrentView = SettingsVM; });
+        }
+    }
+}
---
+++ ModernVPN/MVVM/ViewModel/ProtectionViewModel.cs
@@ -0,0 +1,92 @@
+﻿using ModernVPN.Core;
+using ModernVPN.MVVM.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace ModernVPN.MVVM.ViewModel {
+    public class ProtectionViewModel : ObservableObject {
+
+        public ObservableCollection<ServerModel> Servers { get; set; }
+        private string _сonectionStatus;
+
+        public string ConectionStatus {
+            get {
+                return _сonectionStatus;
+            }
+            set {
+                _сonectionStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand ConectComand { get; set; }
+        public ProtectionViewModel() {
+
+            Servers = new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 30; i++) {
+                Servers.Add(new ServerModel {
+                    Country = "USA"
+                });
+            }
+            ServerBuilder();
+            ConectComand = new RelayCommand(x => {
+                ConectionStatus = "Подключается...";
+                var process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+                process.StartInfo.ArgumentList.Add($"/c rasdial MyServer vpnjantit.com 1 /phonebook:\"C:\\Users\\trofi\\OneDrive\\Рабочий стол\\впн\\ModernVPN\\VPN\\premifr1.vpnjantit.com.pbk\"");
+
+
+                process.Start();
+                process.WaitForExit();
+                switch (process.ExitCode) {
+
+                    case 0:
+                        Debug.WriteLine("Все ок");
+                        ConectionStatus = "Подключено";
+                        break;
+                    case 691:
+                        Debug.WriteLine("691 не Все ок");
+                        ConectionStatus = "не Подключено";
+                        break;
+                    default:
+                        Debug.WriteLine($"{process.ExitCode} не Все ок");
+                        ConectionStatus = $"{process.ExitCode} не Все ок";
+                        break;
+                }
+
+            });
+
+        }
+
+        private void ServerBuilder() {
+            var address = "premifr1.vpnjantit.com";
+            var FolderPath = $"{Directory.GetCurrentDirectory()}/VPN";
+
+            var PdkPath = $"{FolderPath}/{address}.pbk";
+
+            if (!Directory.Exists(FolderPath)) {
+                Directory.CreateDirectory(FolderPath);
+            }
+
+            if (File.Exists(PdkPath)) {
+                MessageBox.Show("Соединение уже существует");
+                return;
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine("[MyServer]");
+            sb.AppendLine("MEDIA=rastapi");
+            sb.AppendLine("Port=VPN2-0");
+            sb.AppendLine("Devise=WAN Miniport (IKEv2)");
+            sb.AppendLine("DEVISE=vpn ");
+            sb.AppendLine($"PhoneNumber={address}");
+            File.WriteAllText(PdkPath, sb.ToString());
+
+        }
+    }
+}
